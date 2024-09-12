f57209f68d45682f9929d545180985acc72fefd3
yomi4486 <dovahkiin4486@icloud.com>
2024-09-12 10:23:51 +0900

    first commit

---
+++ lib/account_startup.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+class AccountStartup extends StatelessWidget{
+  AccountStartup(this.message,this.userCredential);
+  String message;
+  UserCredential userCredential;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DecoratedBox(
+        decoration: const BoxDecoration(color: Color.fromARGB(255, 22, 22, 22)),
+        child:SizedBox(
+          width:MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network("${userCredential.user!.photoURL}"),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+
+                child: Text(
+                  "$message${userCredential.user!.displayName}さん。",
+                  style:(
+                    const TextStyle(
+                      color: Color.fromARGB(255, 240, 240, 240),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    )
+                  )
+                ),
+
+              ),
+            ]
+          )
+        )
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:xero_talk/account_startup.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 22, 22, 22)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<MyHomePage> {
+  void signInWithGoogle() async {
+    try {
+      //Google認証フローを起動する
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      //リクエストから認証情報を取得する
+      final googleAuth = await googleUser?.authentication;
+      //firebaseAuthで認証を行う為、credentialを作成
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      //作成したcredentialを元にfirebaseAuthで認証を行う
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+	  
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        print("loggin OK ,1"); // 新規ユーザーの場合
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => AccountStartup("はじめまして！",userCredential)),
+        );
+      } else {
+        print("loggin OK ,2"); //既存ユーザーの場合
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => AccountStartup("おかえりなさい！",userCredential)),
+        );
+      }
+    } on FirebaseException catch (e) {
+      print(e.message);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DecoratedBox(
+        decoration: const BoxDecoration(color: Color.fromARGB(255, 22, 22, 22)),
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child:SizedBox(
+          
+          width:MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              Image.asset('assets/images/logo.png', fit: BoxFit.contain,width:MediaQuery.of(context).size.width *0.5),
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: const Text("Xero Talkで話そう､繋がろう!",
+                  style:(
+                    TextStyle(
+                      color: Color.fromARGB(255, 240, 240, 240),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    )
+                  )
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.all(16),
+                child:ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 231, 231, 231),
+                    foregroundColor: Colors.black,
+                    shape: const StadiumBorder(),
+                    elevation: 0, // Shadow elevation
+                    shadowColor: const Color.fromARGB(255, 255, 255, 255), // Shadow color
+                  ),
+                  onPressed: () {
+                    try{
+                      signInWithGoogle();
+                    }catch(e){
+                      print(e);
+                    }
+
+                  },
+                  icon: const ImageIcon(
+                    AssetImage("assets/images/google_logo.png")
+                  ),
+                  label: const Text(
+                    'Googleでログイン',
+                    style:(
+                      TextStyle(
+                        color: Colors.black,
+                        fontSize: 16
+                      )
+                    )
+                  ),
+
+                ),
+              )
+              
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
