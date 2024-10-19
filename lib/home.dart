f054aef35d355601eb5803dcc8d7ff34188f7fbf
yomi4486 <dovahkiin4486@icloud.com>
2024-10-19 17:09:14 +0900

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:xero_talk/chat.dart';
 class chatHome extends StatelessWidget{
   chatHome(this.userCredential);
   UserCredential userCredential;
@@ -57,140 +58,151 @@ class chatHome extends StatelessWidget{
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children:[
-          Stack(clipBehavior: Clip.none,
-          
-          children:[
-            DecoratedBox(
-              decoration: const BoxDecoration(color: Color.fromARGB(255, 22, 22, 22)),
-                child:Column( 
-                  children: [
-                    SizedBox(
-                      width:MediaQuery.of(context).size.width,
-                      child: Container(
-                        margin: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.25,top: 30,right: 30,bottom: 30),
-                        child:Column(
-                          children:[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Column(
-                                  children: [
-                                    Container(
-                                      margin: const EdgeInsets.only(bottom:10),
-                                      child: Row(
-                                        children:[
-                                          ClipRRect( // アイコン表示（角丸）
-                                            borderRadius: BorderRadius.circular(2000000),
-                                              child:Image.network(
-                                                "${userCredential.user!.photoURL}",
-                                                width: MediaQuery.of(context).size.height *0.05,
-                                              ),
-                                          ),
-                                          Container(
-                                            margin: const EdgeInsets.only(left:10),
-                                            child:const Column(
-                                              mainAxisAlignment: MainAxisAlignment.start,
-                                              children:[
-                                                SizedBox(
-                                                  child:Text("yomi4486",style:TextStyle(color:Color.fromARGB(200, 255, 255, 255),fontWeight: FontWeight.bold,),textAlign: TextAlign.left,),
-                                                ),
-                                                // SizedBox(
-                                                //   child:Text("あなた: こんにちは！",style:TextStyle(color:Color.fromARGB(200, 255, 255, 255)),textAlign: TextAlign.left), 
-                                                // )
-                                              ]
-                                            )
-                                          )
-                                        ]
-                                      ),
-                                    ),
-                                    Container(margin: const EdgeInsets.only(bottom:10,top: 10),
-                                      child: Row(
-                                        children: [
-                                          ClipRRect( // アイコン表示（角丸）
-                                            borderRadius: BorderRadius.circular(2000000),
-                                              child:Image.network(
-                                                "${userCredential.user!.photoURL}",
-                                                width: MediaQuery.of(context).size.height *0.05,
-                                              ),
-                                          ),
-                                          Container(
-                                            margin: const EdgeInsets.only(left:10),
-                                            child:const Column(
-                                              mainAxisAlignment: MainAxisAlignment.start,
-                                              children:[
-                                                SizedBox(
-                                                  child:Text("yomi4486",style:TextStyle(color:Color.fromARGB(200, 255, 255, 255),fontWeight: FontWeight.bold,),textAlign: TextAlign.left,),
-                                                ),
-                                                // SizedBox(
-                                                //   child:Text("あなた: こんにちは！",style:TextStyle(color:Color.fromARGB(200, 255, 255, 255)),textAlign: TextAlign.left), 
-                                                // )
-                                              ]
-                                            )
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ],                             
-                                ),
-                              ],
-                            ),
-                          ]
-                        )
-                      ),
-                    ),
-                  ] //childlen 画面全体
-                )
-            ),
-            Positioned(
-              top: 0,
-              left: 0,
-              child: DecoratedBox(
-                decoration: const BoxDecoration(color: Color.fromARGB(255, 68, 68, 68)),
-                
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.2,
-                  height: MediaQuery.of(context).size.height,
-                  child: Column(
+          Stack(
+            clipBehavior: Clip.none,
+            children:[
+              DecoratedBox(
+                decoration: const BoxDecoration(color: Color.fromARGB(255, 22, 22, 22)),
+                  child:Column( 
                     children: [
-                      Container(
-                        margin: const EdgeInsets.all(12),
-                        child:ClipRRect( // アイコン表示（角丸）
-                          borderRadius: BorderRadius.circular(2000000),
-                          child:Container(
-                            color:const Color.fromARGB(255, 140, 206, 74),
-                            child:Image.asset(
-                              "assets/images/chat.png",
-                              width: MediaQuery.of(context).size.width *0.15,
-                            )
-                          ),
-                        )
+                      SizedBox(
+                        width:MediaQuery.of(context).size.width,
+                        child: Container(
+                          margin: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.25,top: 30,right: 30,bottom: 30),
+                          child:Column(
+                            children:[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Column(
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          print("onTap called.");
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(builder: (context) => chat(userCredential)),
+                                          );
+
+                                        },
+                                        child:Container(
+                                          decoration: const BoxDecoration(color:Color.fromARGB(0, 255, 255, 255)),
+                                          margin: const EdgeInsets.only(bottom:10),
+                                          child: Row(
+                                            children:[
+                                              ClipRRect( // アイコン表示（角丸）
+                                                borderRadius: BorderRadius.circular(2000000),
+                                                  child:Image.network(
+                                                    "${userCredential.user!.photoURL}",
+                                                    width: MediaQuery.of(context).size.height *0.05,
+                                                  ),
+                                              ),
+                                              Container(
+                                                margin: const EdgeInsets.only(left:10),
+                                                child:const Column(
+                                                  mainAxisAlignment: MainAxisAlignment.start,
+                                                  children:[
+                                                    SizedBox(
+                                                        child:Text("yomi4486",style:TextStyle(color:Color.fromARGB(200, 255, 255, 255),fontWeight: FontWeight.bold,),textAlign: TextAlign.left,),
+                                                    ),
+                                                    // SizedBox(
+                                                    //   child:Text("あなた: こんにちは！",style:TextStyle(color:Color.fromARGB(200, 255, 255, 255)),textAlign: TextAlign.left), 
+                                                    // )
+                                                  ]
+                                                )
+                                              )
+                                            ]
+                                          ),
+                                        ),
+                                      ),
+                                      Container(margin: const EdgeInsets.only(bottom:10,top: 10),
+                                        child: Row(
+                                          children: [
+                                            ClipRRect( // アイコン表示（角丸）
+                                              borderRadius: BorderRadius.circular(2000000),
+                                                child:Image.network(
+                                                  "${userCredential.user!.photoURL}",
+                                                  width: MediaQuery.of(context).size.height *0.05,
+                                                ),
+                                            ),
+                                            Container(
+                                              margin: const EdgeInsets.only(left:10),
+                                              child:const Column(
+                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                children:[
+                                                  SizedBox(
+                                                    child:Text("yomi4486",style:TextStyle(color:Color.fromARGB(200, 255, 255, 255),fontWeight: FontWeight.bold,),textAlign: TextAlign.left,),
+                                                  ),
+                                                  // SizedBox(
+                                                  //   child:Text("あなた: こんにちは！",style:TextStyle(color:Color.fromARGB(200, 255, 255, 255)),textAlign: TextAlign.left), 
+                                                  // )
+                                                ]
+                                              )
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ],                             
+                                  ),
+                                ],
+                              ),
+                            ]
+                          )
+                        ),
                       ),
-                      Container(
-                        margin: const EdgeInsets.all(12),
-                        child:ClipRRect( // アイコン表示（角丸）
-                          borderRadius: BorderRadius.circular(2000000),
-                          child:Image.asset(
-                            "assets/images/logo.png",
-                            width: MediaQuery.of(context).size.width *0.15,
-                          ),
-                        )
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(12),
-                        child:ClipRRect( // アイコン表示（角丸）
-                          borderRadius: BorderRadius.circular(2000000),
-                          child:Image.asset(
-                            "assets/images/logo.png",
-                            width: MediaQuery.of(context).size.width *0.15,
-                          ),
-                        )
-                      ),
-                    ],
+                    ] //childlen 画面全体
                   )
+              ),
+              Positioned(
+                top: 0,
+                left: 0,
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(color: Color.fromARGB(255, 68, 68, 68)),
+                  
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    height: MediaQuery.of(context).size.height,
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(12),
+                          child:ClipRRect( // アイコン表示（角丸）
+                            borderRadius: BorderRadius.circular(2000000),
+                            child:Container(
+                              color:const Color.fromARGB(255, 140, 206, 74),
+                              child:Image.asset(
+                                "assets/images/chat.png",
+                                width: MediaQuery.of(context).size.width *0.15,
+                              )
+                            ),
+                          )
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(12),
+                          child:ClipRRect( // アイコン表示（角丸）
+                            borderRadius: BorderRadius.circular(2000000),
+                            child:Image.asset(
+                              "assets/images/logo.png",
+                              width: MediaQuery.of(context).size.width *0.15,
+                            ),
+                          )
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(12),
+                          child:ClipRRect( // アイコン表示（角丸）
+                            borderRadius: BorderRadius.circular(2000000),
+                            child:Image.asset(
+                              "assets/images/logo.png",
+                              width: MediaQuery.of(context).size.width *0.15,
+                            ),
+                          )
+                        ),
+                      ],
+                    )
+                  ),
                 ),
               ),
-            ),
-          ]
+            ]
           )
         ],
       )
