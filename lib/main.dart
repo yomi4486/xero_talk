ade42323f4706a47ee0a7591101f95d72d090c5b
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2024-11-28 13:49:12 +0900

    変数の名前やコードのバグを修正

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:xero_talk/account_startup.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:xero_talk/home.dart';
 import 'dart:io';
