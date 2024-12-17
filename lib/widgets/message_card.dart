4c936b89c63c0bcf1906f964343cad19c0d4c079
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2024-12-17 22:16:58 +0900

    不要なimportを削除、送信ボタンをグラデーションにした

--- lib/widgets/message_card.dart
+++ lib/widgets/message_card.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:convert' as convert;
-import 'package:rxdart/rxdart.dart';
 
 List<Widget> returnWidget = [];
 
