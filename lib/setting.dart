41733e110500e8b02370aa60b537558d7e9a1e35
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-01-31 14:43:49 +0900

    setting.dartをscaffoldに変更

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:xero_talk/home.dart';
-import 'package:xero_talk/notify.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:xero_talk/utils/auth_context.dart';
 import 'package:xero_talk/widgets/setting_item.dart';
@@ -24,9 +22,8 @@ class _SettingPage extends State<SettingPage>{
   @override
   Widget build (BuildContext context) {
     var profile = instance.userCredential.additionalUserInfo?.profile;
-    return WillPopScope(
-      onWillPop:() async => true,
-      child: FutureBuilder(
+    return Scaffold(
+      body: FutureBuilder(
         future:FirebaseFirestore.instance
           .collection('user_account') // コレクションID
           .doc('${profile?["sub"]}') // ドキュメントID
@@ -41,76 +38,15 @@ class _SettingPage extends State<SettingPage>{
             }
           }
           return Scaffold(
-            bottomNavigationBar: BottomNavigationBar(
-              enableFeedback:false,
-              currentIndex:2,
-              onTap: (value) {
-                if(value == 0){
-                  Navigator.push(context, PageRouteBuilder(
-                    pageBuilder: (_, __, ___)=>chatHome(),
-                      transitionsBuilder: (context, animation, secondaryAnimation, child){
-                      return FadeTransition(opacity: animation, child: child,);
-                    }
-                  ));
-                }else if(value==1){
-                  Navigator.push(context, PageRouteBuilder(
-                    pageBuilder: (_, __, ___)=>NotifyPage(),
-                      transitionsBuilder: (context, animation, secondaryAnimation, child){
-                      return FadeTransition(opacity: animation, child: child,);
-                    }
-                  ));
-                }
-              },
-              unselectedLabelStyle: const TextStyle(color: Color.fromARGB(255, 200, 200, 200)),
-              unselectedItemColor: const Color.fromARGB(255, 200, 200, 200),
-              selectedLabelStyle: const TextStyle(color: Color.fromARGB(255, 140, 206, 74)),
-              selectedItemColor: const Color.fromARGB(255, 140, 206, 74),
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'ホーム',
-                  
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.notifications),
-                  label: '通知',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person,),
-                  label:'アカウント' ,
-                  
-                ),
-              ],
-              backgroundColor: const Color.fromARGB(255, 40, 40, 40),
-            ),
             appBar:AppBar(
               centerTitle: false,
               automaticallyImplyLeading: false,
-              title: const Text("アカウント",style: TextStyle(fontWeight: FontWeight.bold,)),
+              title: const Text("設定",style: TextStyle(fontWeight: FontWeight.bold,)),
               titleTextStyle: const TextStyle(
                 color:Color.fromARGB(255, 255, 255, 255),
                 fontSize: 20
               ),
               backgroundColor: const Color.fromARGB(255, 40, 40, 40),
-              actions: [  
-                              
-                Container( 
-                  padding: const EdgeInsets.only(right: 10), 
-                  child:ClipRRect( // アイコン表示（角丸）
-                    borderRadius: BorderRadius.circular(30),
-                    child:Container(
-                      color:const Color.fromARGB(0, 255, 255, 255),
-                      child:IconButton(
-                        onPressed: (){}, // TODO:setting.dartに遷移する処理を書く
-                        icon: const Icon(
-                          Icons.settings,
-                          color: Color.fromARGB(128, 255, 255, 255)
-                        )
-                      )
-                    ),
-                  ),
-                ),
-              ],
             ),
             floatingActionButton: _showFab ? FloatingActionButton( 
               onPressed: () async {
