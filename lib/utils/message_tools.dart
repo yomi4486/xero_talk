f2084400e1bf7f1288861306c32e5845e8fa86c8
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-02-03 22:53:02 +0900

    各画面に配置されているアイコンにテーマを適応させるようにした

--- lib/utils/message_tools.dart
+++ lib/utils/message_tools.dart
@@ -38,17 +38,19 @@ Future deleteMessage(String messageId,String channelId) async {
 }
 
 Future editMessage(String messageId,String channelId,String content) async {
-  final sendBody = {"type": "edit_message","id": messageId,"channel":channelId,"content":content};
-  final String data = convert.json.encode(sendBody);
-  if(instance.channel.readyState == 3){ // WebSocketが接続されていない場合
-    await instance.restoreConnection().then((v){
+  if (content.isNotEmpty) {
+    final sendBody = {"type": "edit_message","id": messageId,"channel":channelId,"content":content};
+    final String data = convert.json.encode(sendBody);
+    if(instance.channel.readyState == 3){ // WebSocketが接続されていない場合
+      await instance.restoreConnection().then((v){
+        instance.channel.add(data);
+      });
+      return;
+    }
+    try{
       instance.channel.add(data);
-    });
-    return;
-  }
-  try{
-    instance.channel.add(data);
-  }catch(e){
-    print('編集に失敗：${e}');
+    }catch(e){
+      print('編集に失敗：${e}');
+    }
   }
 }
