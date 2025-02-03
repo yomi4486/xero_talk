e5f9c13bfb69f9a91aebd9bb01531371a7cc30b7
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-02-03 15:56:35 +0900

    編集機能実装完了🩷

--- lib/widgets/message_card.dart
+++ lib/widgets/message_card.dart
@@ -84,7 +84,7 @@ class _MessageCardState extends State<MessageCard> {
     } 
   }
 
-  List<TextSpan> getTextSpans(String text) {
+  List<TextSpan> getTextSpans(String text,bool edited,List<Color>textColor) {
     final RegExp urlRegExp = RegExp(
       r'(http|https):\/\/([\w.]+\/?)\S*',
       caseSensitive: false,
@@ -102,18 +102,27 @@ class _MessageCardState extends State<MessageCard> {
       spans.add(
         TextSpan(
           text: url,
-            style: const TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
-            recognizer: TapGestureRecognizer()..onTap = () async {
-              if (await canLaunch(url!)) {
-                await launch(url);
-              }
-            },
-          ),
+          style: const TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
+          recognizer: TapGestureRecognizer()..onTap = () async {
+            if (await canLaunch(url!)) {
+              await launch(url);
+            }
+          },
+        ),
       );
       lastMatchEnd = match.end;
     }
     if (lastMatchEnd < text.length) {
       spans.add(TextSpan(text: text.substring(lastMatchEnd)));
+    }
+    if(edited){
+      print("test");
+      spans.add(
+        TextSpan(
+          text: " (編集済み)",
+          style: TextStyle(color:textColor[0],fontSize: 10),
+        ),
+      );
     }
     return spans;
   }
@@ -170,192 +179,194 @@ class _MessageCardState extends State<MessageCard> {
               displayName = (docSnapshot.data?.data() as Map<String, dynamic>)["display_name"] ?? "No Name";
               final String type = content["type"];
               final String messageId = content["id"];
+              bool edited = false;
               if(type == "delete_message"){
                 removeWidget(messageId);
                 return Column(children: returnWidget);
               }
               if(type == "edit_message"){
                 editWidget(messageId,content["content"]);
-                return Column(children: returnWidget);
+                edited = true;
               }
               final String messageContent = content["content"];
               final int timestamp = content["timestamp"];
               final DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(timestamp);
               final String modifiedDateTime = getTimeStringFormat(dateTime);
-              
-              if(lastMessageId == messageId){ // 同一のメッセージ複数受け取っている場合は無視
+              if(type == "send_message" && lastMessageId == messageId){
                 return Column(children: returnWidget,);
               }
               lastMessageId = messageId; // 最終受信を上書き
               chatHistory[messageId] = {
                 "author":content["author"],
                 "content":messageContent,
-                "id":messageId,
                 "timeStamp":timestamp,
                 "display_time":modifiedDateTime,
-                "edited":false,
+                "edited":edited,
                 "display_name":displayName
               };
-              final Widget _chatWidget = Container(
-                margin: const EdgeInsets.only(bottom: 10, top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(2000000),
-                      child: Image.network(
-                        "https://${dotenv.env['BASE_URL']}:8092/geticon?user_id=${chatHistory[messageId]["author"]}",
-                        width: MediaQuery.of(context).size.height * 0.05,
+              returnWidget = [];
+              for (var entry in chatHistory.entries){
+                final Widget _chatWidget = Container(
+                  margin: const EdgeInsets.only(bottom: 10, top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(2000000),
+                        child: Image.network(
+                          "https://${dotenv.env['BASE_URL']}:8092/geticon?user_id=${entry.value["author"]}",
+                          width: MediaQuery.of(context).size.height * 0.05,
+                        ),
                       ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children:[
-                              Text( // 名前
-                                chatHistory[messageId]["display_name"],
-                                style: TextStyle(
-                                  color: textColor[2],
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14,
-                                ),
-                                textAlign: TextAlign.left,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 7),
-                                child:Text( // 時刻
-                                  chatHistory[messageId]["display_time"],
+                      Container(
+                        margin: const EdgeInsets.only(left: 10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children:[
+                                Text( // 名前
+                                  entry.value["display_name"],
                                   style: TextStyle(
-                                    color: textColor[0],
+                                    color: textColor[2],
                                     fontWeight: FontWeight.bold,
-                                    fontSize: 10,
-                                    overflow: TextOverflow.ellipsis,
+                                    fontSize: 14,
                                   ),
+                                  textAlign: TextAlign.left,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 7),
+                                  child:Text( // 時刻
+                                    entry.value["display_time"],
+                                    style: TextStyle(
+                                      color: textColor[0],
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 10,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  )
                                 )
-                              )
-                            ]
-                          ),
-                          Row(
-                            children:[
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width*0.7,
-                                child:RichText(
-                                  text: TextSpan(
-                                    children: getTextSpans(chatHistory[messageId]["content"]),
-                                    style:TextStyle(
-                                      color: textColor[1],
-                                      fontSize: 16.0
+                              ]
+                            ),
+                            Row(
+                              children:[
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width*0.7,
+                                  child:RichText(
+                                    text: TextSpan(
+                                      children: getTextSpans(entry.value["content"],entry.value["edited"],textColor),
+                                      style:TextStyle(
+                                        color: textColor[1],
+                                        fontSize: 16.0
+                                      ),
                                     ),
                                   ),
-                                ),),
-                                chatHistory[messageId]["edited"] ? Padding(padding:const EdgeInsets.only(left:20),child:Text("編集済み",style:TextStyle(color: textColor[0]))) : Container()
+                                ),
                               ]
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-              final chatWidget = GestureDetector(
-                key:ValueKey(messageId),
-                onLongPress: (){
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20.0),
-                            topRight: Radius.circular(20.0),
-                          ),
+                            ),
+                          ],
                         ),
-                        height: MediaQuery.of(context).size.height*0.4,
-                        child: Padding(
-                          padding: const EdgeInsets.only(top:20),
-                          child:ListView(
-                            children: [
-                              SimpleDialogOption( // メッセージ削除ボタン
-                                padding: const EdgeInsets.all(15),
-                                child: const Row(
-                                  children:[
-                                    Icon(Icons.delete),
-                                    Padding(
-                                      padding: EdgeInsets.only(left:5),
-                                      child: Text('メッセージを削除',style: TextStyle(fontSize: 16))
-                                    )
-                                  ]
-                                ),
-                                onPressed: ()async {
-                                  Navigator.pop(context);
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return SimpleDialog(
-                                        title:const Text('メッセージを削除',style: TextStyle(fontSize: 16),),
-                                        children: <Widget>[
-                                          SizedBox(
-                                            width: MediaQuery.of(context).size.width *0.8,
-                                            child:Padding(
-                                              padding:const EdgeInsets.all(10),
-                                              child:_chatWidget,
+                      ),
+                    ],
+                  ),
+                );
+                final chatWidget = GestureDetector(
+                  key:ValueKey(entry.key),
+                  onLongPress: (){
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20.0),
+                              topRight: Radius.circular(20.0),
+                            ),
+                          ),
+                          height: MediaQuery.of(context).size.height*0.4,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top:20),
+                            child:ListView(
+                              children: [
+                                SimpleDialogOption( // メッセージ削除ボタン
+                                  padding: const EdgeInsets.all(15),
+                                  child: const Row(
+                                    children:[
+                                      Icon(Icons.delete),
+                                      Padding(
+                                        padding: EdgeInsets.only(left:5),
+                                        child: Text('メッセージを削除',style: TextStyle(fontSize: 16))
+                                      )
+                                    ]
+                                  ),
+                                  onPressed: ()async {
+                                    Navigator.pop(context);
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return SimpleDialog(
+                                          title:const Text('メッセージを削除',style: TextStyle(fontSize: 16),),
+                                          children: <Widget>[
+                                            SizedBox(
+                                              width: MediaQuery.of(context).size.width *0.8,
+                                              child:Padding(
+                                                padding:const EdgeInsets.all(10),
+                                                child:_chatWidget,
+                                              ),
                                             ),
-                                          ),
-                                          SimpleDialogOption(
-                                            child: const Text('削除',style: TextStyle(color: Color.fromARGB(255, 255, 10, 10)),),
-                                            onPressed: ()async {
-                                              await deleteMessage(messageId, widget.channelInfo["id"]);
-                                              Navigator.pop(context);
-                                            }
-                                          ),
-                                          SimpleDialogOption(
-                                            child: const Text('キャンセル'),
-                                            onPressed: ()async{
-                                              Navigator.pop(context);
-                                            }
-                                          ),
-                                        ],
-                                      );
-                                    },
-                                  );      
-                                }
-                              ),
-                              SimpleDialogOption( // メッセージ削除ボタン
-                                padding: const EdgeInsets.all(15),
-                                child: const Row(
-                                  children:[
-                                    Icon(Icons.edit),
-                                    Padding(
-                                      padding: EdgeInsets.only(left:5),
-                                      child: Text('編集',style: TextStyle(fontSize: 16))
-                                    )
-                                  ]
+                                            SimpleDialogOption(
+                                              child: const Text('削除',style: TextStyle(color: Color.fromARGB(255, 255, 10, 10)),),
+                                              onPressed: ()async {
+                                                await deleteMessage(entry.key, widget.channelInfo["id"]);
+                                                Navigator.pop(context);
+                                              }
+                                            ),
+                                            SimpleDialogOption(
+                                              child: const Text('キャンセル'),
+                                              onPressed: ()async{
+                                                Navigator.pop(context);
+                                              }
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );      
+                                  }
                                 ),
-                                onPressed: ()async {
-                                  Navigator.pop(context);
-                                  widget.focusNode.requestFocus();
-                                  widget.fieldText.text = messageContent;
-                                  widget.EditMode(messageId,true);
-                                }
-                              ),
-                            ],
+                                SimpleDialogOption( // メッセージ削除ボタン
+                                  padding: const EdgeInsets.all(15),
+                                  child: const Row(
+                                    children:[
+                                      Icon(Icons.edit),
+                                      Padding(
+                                        padding: EdgeInsets.only(left:5),
+                                        child: Text('編集',style: TextStyle(fontSize: 16))
+                                      )
+                                    ]
+                                  ),
+                                  onPressed: ()async {
+                                    Navigator.pop(context);
+                                    widget.focusNode.requestFocus();
+                                    widget.fieldText.text = entry.value["content"];
+                                    widget.EditMode(entry.key,true);
+                                  }
+                                ),
+                              ],
+                            )
                           )
-                        )
-                      );
-                    },
-                  );
-                },
-                child: _chatWidget
-              );
-              addWidget(chatWidget,_currentPosition);
+                        );
+                      },
+                    );
+                  },
+                  child: _chatWidget
+                );
+                addWidget(chatWidget,_currentPosition);
+              }
             }
             return Column(children: returnWidget);
           },
