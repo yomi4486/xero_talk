2c5ee8d1b367d7a57a8596b49e596bcc6ddb6f91
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-04-01 17:29:31 +0900

    Add: call logic

--- lib/utils/voice_chat.dart
+++ lib/utils/voice_chat.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:xero_talk/utils/auth_context.dart';
-import 'dart:convert';
+import 'dart:convert' as convert;
 import 'package:uuid/uuid.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,7 +16,43 @@ Future<String> createRoom() async {
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8'
       },
-      body: jsonEncode(<String, String>{'token': token!}));
+      body: convert.jsonEncode(<String, String>{'token': token!}));
+  if (response.statusCode != 200) {
+    print('Request failed with status: ${response.statusCode}');
+  }
+  return response.body.toString();
+}
+
+void call(String to_user_id) async {
+  final sendBody = {
+    "type": "call",
+    "author": instance.id,
+    "channel": to_user_id
+  };
+  final String data = convert.json.encode(sendBody);
+  if (instance.channel.readyState == 3) {
+    // WebSocketが接続されていない場合
+    await instance.restoreConnection();
+    instance.channel.add(data);
+    return;
+  }
+  try {
+    instance.channel.add(data);
+  } catch (e) {
+    print('送信に失敗：${e}');
+  }
+}
+
+Future<String> getRoom(String roomId) async {
+  String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
+  final response = await http.get(
+      Uri.parse("https://${dotenv.env['BASE_URL']}/voiceclient"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'token':token!,
+        'room_id':roomId
+      }
+  );
   if (response.statusCode != 200) {
     print('Request failed with status: ${response.statusCode}');
   }
