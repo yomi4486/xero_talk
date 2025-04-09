89f090ee615555736e0e81a4808c34317ad80dea
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-04-10 01:13:17 +0900

    Delete: Audio Session  Native Code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,19 +8,6 @@ import AVFoundation
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
-    // オーディオセッションを設定
-    let audioSession = AVAudioSession.sharedInstance()
-    do {
-        try audioSession.setCategory(.playAndRecord, options: [.defaultToSpeaker, .allowBluetooth])
-        try audioSession.setActive(true)
-    } catch {
-        print("Audio session setup failed: \(error)")
-    }
-    
-    // リモートコントロールイベントの受信を開始
-    application.beginReceivingRemoteControlEvents()
-    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
